d811d6b8c3848248e3d14c9f7cefbd010954eb45
birkin <birkin.diana@gmail.com>
2020-07-05 12:51:28 -0400

    initial commit; lists marc files for given path.

---
+++ exp01_list_marc_files/src/main.rs
@@ -0,0 +1,31 @@
+extern crate glob;  // <https://docs.rs/glob/0.3.0/glob/>
+
+// use std::fmt::Debug;
+// use std::fmt::Display;
+
+use glob::glob;
+
+
+fn main() {
+
+    let file_download_dir: String = "../../source_files".to_string();
+    let pattern: String = format!( "{}/*.mrc", file_download_dir );
+    println!("pattern, ``{:?}``", pattern);
+
+    let paths = glob( &pattern ).unwrap_or_else( |err| {
+        panic!("could not glob the pattern; error, ``{}``", err);
+    });
+    // let zz: () = paths;  // yields: expected `()`, found struct `glob::Paths`
+    // println!("paths, ``{:?}``", paths);  // error: `glob::Paths` cannot be formatted using `{:?}` because it doesn't implement `std::fmt::Debug`
+    // println!("paths, ``{}``", paths);  // error: `glob::Paths` cannot be formatted with the default formatter
+
+    let iter: Vec<glob::GlobResult> = paths.into_iter().collect();  // <https://docs.rs/glob/0.3.0/glob/type.GlobResult.html>
+    // let zz: () = iter;  // yields: expected `()`, found struct `std::vec::Vec`
+    println!( "iter, ``{:?}``", iter );
+}
+
+
+// -- Output...
+
+// pattern, ``"../../source_files/*.mrc"``
+// iter, ``[Ok("../../source_files/sierra_export_0726.mrc")]``
