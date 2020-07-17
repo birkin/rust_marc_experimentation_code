b60e622d3edb20f65038ec09585fde68f987b172
birkin <birkin.diana@gmail.com>
2020-07-17 07:47:45 -0400

    implements python version.

--- pytry/marc_read.py
+++ pytry/marc_read.py
@@ -8,14 +8,33 @@ FILES_DIR = './source_files'
 
 def process_marc_files():
 
+    file_start_time = datetime.datetime.now()
+
     ## get list of files
     marc_file_list: list = create_marc_file_list()
 
     ## for each file...
+    for marc_path in marc_file_list:
 
-        ## assemble full-title & print it
+        file_start_time = datetime.datetime.now()
+        print( '\nnew file...' )
+        print( f'marc_file_path, ``{marc_path}``' )
 
-        ## assemble bib-url & print it
+        with open( marc_path, 'rb' ) as fh:
+            reader = MARCReader( fh )
+            for record in reader:
+                print( '\nnew record...' )
+
+                print( f'full_title, ``{record.title()}``'  )
+
+                bib = record['907']['a']
+                print( f'bib_url, ``https://search.library.brown.edu/catalog/{bib[1:-1]}``' )
+
+        file_end_time = datetime.datetime.now()
+        print( f'\nfile-elapsed-time, ``{file_end_time - file_start_time}``' )
+
+    all_files_end_time = datetime.datetime.now()
+    print( f'\nall-files-elapsed-time, ``{all_files_end_time - file_start_time}``\n' )
 
     ## end process_marc_files()
 
@@ -24,40 +43,6 @@ def create_marc_file_list():
     marc_file_list = sorted( glob.glob('%s/*.mrc' % FILES_DIR) )
     print( f'marc_file_list, ``{pprint.pformat(marc_file_list)}``' )
     return marc_file_list
-
-
-
-def validate_marc_files():
-    """ Checks downloaded marc files. """
-    marc_file_list = sorted( glob.glob('%s/*.mrc' % FILE_DOWNLOAD_DIR) )
-    print( f'marc_file_list, ``{marc_file_list}``' )
-    start = datetime.datetime.now()
-    for file_path in marc_file_list:
-        print( f'file_path, ``{file_path}``' )
-        size_in_mb = os.path.getsize( file_path ) / 1024 / 1024
-        print( f'size, ``{size_in_mb}MB``' )
-        validity = open_and_check_file( file_path )
-        print( f'validity, ``{validity}``' )
-    time_taken = str( datetime.datetime.now() - start )
-    print( f'time_taken, ``{time_taken}``' )
-    return
-
-def open_and_check_file( file_path ):
-    """ Opens file and ensures it can be read as marc.
-        Called by validate_marc_files() """
-    validity = False
-    titles = []
-    print( f'file about to be checked, ``{file_path}``' )
-    with open( file_path, 'rb' ) as fh:
-        reader = MARCReader( fh )
-        try:
-            for record in reader:
-                titles.append( record.title() )
-            print( f'titles, ``{pprint.pformat(titles)}``' )
-            validity = True
-        except Exception as e:
-            print( f'problem reading file, ``{e}``' )
-    return validity
 
 
 if __name__ == '__main__':
