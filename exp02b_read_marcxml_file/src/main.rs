818771b600c7121e993546e38e88348247dfe931
Birkin James Diana <birkin.diana@gmail.com>
2023-05-03 22:36:10 -0400

    uses logging instead of print statements.

--- exp02b_read_marcxml_file/src/main.rs
+++ exp02b_read_marcxml_file/src/main.rs
@@ -1,16 +1,16 @@
 use marc::*;
-use log::{debug, error, info, trace, warn};
+use log::*;
 use simple_logger::SimpleLogger;
 
 
 fn main() {
 
-    /// -- init logging
+    // -- init logging
     SimpleLogger::new().init().unwrap();  // or, to set the mininum level: ```SimpleLogger::new().with_level(log::LevelFilter::Info).init().unwrap();```
     
-    /// -- get marc file path
+    // -- get marc file path
     let marc_path: String = "./source_files/sample_bibs_2022050222_7532401250006966_new_99.xml".to_string();
     debug!( "marc_path, ``{:?}``", marc_path);
         
-    println!("Hello, world!");
+    info!("end of main()");
 }
